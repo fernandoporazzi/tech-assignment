1d51a556c824af87ef235e96f0bf255087611c20
Fernando <fernando@MacBook-Pro-van-ibood.local>
2019-11-28 09:25:20 +0100

    Use Algolia API instead of Firebase API

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -1,8 +1,34 @@
+import Link from 'next/link';
+
 const Layout = (props) => {
   return (
     <>
       <header>
-        header
+        <div>
+          Hacker News
+        </div>
+        <ul>
+          <li>
+            <Link href="/news">
+              <a>new</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/front">
+              <a>past</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/newcomments">
+              <a>comments</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/ask">
+              <a>ask</a>
+            </Link>
+          </li>
+        </ul>
       </header>
 
       <main>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import fetch from 'isomorphic-unfetch'
 import Layout from '../components/layout/layout';
 
-const Home = ({items}) => {
+const Home = ({ data }) => {
   return (
     <Layout>
       <ul>
-        {items.map(it => {
-          return <li key={it.id}>{it.title}</li>
+        {data.hits.map(it => {
+          return <li key={it.objectID}>{it.title}</li>
         })}
       </ul>
     </Layout>
@@ -14,22 +14,11 @@ const Home = ({items}) => {
 };
 
 Home.getInitialProps = async ({ req }) => {
-  // fetch top stories ids
-  const res = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
-  let items = await res.json();
+  // https://hn.algolia.com/api
+  const res = await fetch('https://hn.algolia.com/api/v1/search?tags=story');
+  const data = await res.json();
 
-  // due to performance issues, I am using only 10 items
-  items = items.slice(0, 10);
-
-  // fetch data for each of the 10 items
-  let d;
-  let promises = items.map(item => fetch(`https://hacker-news.firebaseio.com/v0/item/${item}.json?print=pretty`).then(y => y.json()));
-  
-  await Promise.all(promises).then(results => {
-    d = results;
-  });
-
-  return { items: d };
+  return { data };
 };
 
 export default Home;
