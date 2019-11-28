02138951b901adae3d4fcf0cbd3d2d0ed8c8d24e
Fernando <fernando@MacBook-Pro-van-ibood.local>
2019-11-28 11:06:27 +0100

    Create recursive comments

--- pages/items.jsx
+++ pages/items.jsx
@@ -1,11 +1,35 @@
 import fetch from 'isomorphic-unfetch'
 import Layout from '../components/layout/layout';
 
+const Comment = ({ comment }) => {
+  const nestedComments = (comment.children || []).map(comment => {
+    return <Comment key={comment.id} comment={comment} type="child" />
+  });
+ 
+  return (
+    <div style={{"marginLeft": "25px", "marginTop": "10px"}}>
+      <div style={{color: 'red'}}>{comment.author} - {comment.created_at}</div>
+      <div dangerouslySetInnerHTML={{__html: comment.text}} />
+      {nestedComments}
+    </div>
+  )
+};
+
 const Item = ({ data }) => {
   return (
     <Layout>
       <h2>{data.title}</h2>
-      {data.type}
+      
+      <form action="">
+        <textarea></textarea>
+        <button type="submit">add comment</button>
+      </form>
+
+      <div>
+        {data.children.map((item, index) => {
+          return <Comment key={item.id} comment={item} />
+        })}
+      </div>
     </Layout>
   );
 };
